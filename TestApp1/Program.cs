0a2c536cb11323b1e3d682f10806edd746920cd7
Ali <mohamadalitellawi@gmail.com>
2019-11-05 08:33:32 +0300

    update for other types

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace TestApp1
 {
@@ -6,7 +8,28 @@ namespace TestApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Data.DataSet ds;
+
+            System.Net.Http.HttpClient client;
+
+            // loop through the assemblies that this app references
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // load the assembly so we can read details
+                var a = Assembly.Load(r.FullName);
+
+                // count the number of mothods
+                int methodCount = 0;
+                // loop through all types in the assembly
+                foreach (var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+
+                }
+
+                // output the count of types and their methods
+                Console.WriteLine($"{a.DefinedTypes.Count()} types with {methodCount} methods in {r.Name} assembly.");
+            }
         }
     }
 }
