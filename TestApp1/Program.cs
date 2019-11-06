3c5fe7c4d47cc913df08f9811a32430985dd7d41
Ali <mohamadalitellawi@gmail.com>
2019-11-06 09:09:10 +0300

    add logical operator

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using static System.Console;
 
 namespace TestApp1
 {
@@ -11,24 +12,49 @@ namespace TestApp1
             System.Data.DataSet ds;
 
             System.Net.Http.HttpClient client;
-
-            // loop through the assemblies that this app references
-            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
             {
-                // load the assembly so we can read details
-                var a = Assembly.Load(r.FullName);
+                bool a = true;
+                bool b = false;
 
-                // count the number of mothods
-                int methodCount = 0;
-                // loop through all types in the assembly
-                foreach (var t in a.DefinedTypes)
+                WriteLine($"AND | a | b");
+                WriteLine($"a |{a & a,-5}|{a & b,-5}");
+                WriteLine($"b | {b & a,-5} | {b & b,-5} ");
+                WriteLine();
+
+                WriteLine($"OR | a | b ");
+
+                WriteLine($"a | {a | a,-5} | {a | b,-5} ");
+
+                WriteLine($"b | {b | a,-5} | {b | b,-5} ");
+
+                WriteLine();
+
+                WriteLine($"XOR | a | b ");
+
+                WriteLine($"a | {a ^ a,-5} | {a ^ b,-5} ");
+
+                WriteLine($"b | {b ^ a,-5} | {b ^ b,-5} ");
+                WriteLine();
+            }
+            {
+                // loop through the assemblies that this app references
+                foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
                 {
-                    methodCount += t.GetMethods().Count();
+                    // load the assembly so we can read details
+                    var a = Assembly.Load(r.FullName);
 
+                    // count the number of mothods
+                    int methodCount = 0;
+                    // loop through all types in the assembly
+                    foreach (var t in a.DefinedTypes)
+                    {
+                        methodCount += t.GetMethods().Count();
+
+                    }
+
+                    // output the count of types and their methods
+                    Console.WriteLine($"{a.DefinedTypes.Count()} types with {methodCount} methods in {r.Name} assembly.");
                 }
-
-                // output the count of types and their methods
-                Console.WriteLine($"{a.DefinedTypes.Count()} types with {methodCount} methods in {r.Name} assembly.");
             }
         }
     }
